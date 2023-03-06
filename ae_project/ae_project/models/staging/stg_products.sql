b603b90bb1e98edaa909f2f7d395fcf1547b7d41
Meg Wu <megwu1129@gmail.com>
2023-03-06 01:32:10 -0600

    Removed ; issue from supplier_ids

--- ae_project/ae_project/models/staging/stg_products.sql
+++ ae_project/ae_project/models/staging/stg_products.sql
@@ -1,5 +1,21 @@
 WITH source AS (
-    SELECT * FROM {{ source('northwind', 'products')}}
+    SELECT
+        CAST(supplier_ids AS INTEGER) AS supplier_id
+        , id
+        , product_code
+        , product_name
+        , description
+        , standard_cost
+        , list_price
+        , reorder_level
+        , target_level
+        , quantity_per_unit
+        , discontinued
+        , minimum_reorder_quantity
+        , category
+        , attachments
+    FROM {{ source('northwind', 'products')}}
+    WHERE supplier_ids NOT LIKE "%;%"
 )
 
 SELECT
